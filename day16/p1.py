4e3e96507a80309eab33c06bf955561c6e594dda
Daniel Hoang <107479212+danhoangg@users.noreply.github.com>
2024-12-16 13:15:12 +0000

    Update p1.py

--- day16/p1.py
+++ day16/p1.py
@@ -21,13 +21,13 @@ heapq.heappush(pq, (0, *start, 0, 1))
 directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 while pq:
     w, r, c, fr, fc = heapq.heappop(pq)
-    visited.add((r, c, fr, fc))
+    visited.add((r, c))
     if (r, c) == target:
         print(w)
         break
     for dr, dc in directions:
         nr, nc = r + dr, c + dc
-        if nr in range(rows) and nc in range(cols) and (nr, nc, dr, dc) not in visited and grid[nr][nc] == '.':
+        if nr in range(rows) and nc in range(cols) and (nr, nc) not in visited and grid[nr][nc] == '.':
             if (dr, dc) == (fr, fc):
                 heapq.heappush(pq, (w + 1, nr, nc, dr, dc))
             else:
